41e21e4fa0cac38385294e5b15019fadf3a29d65
Jared Quinn <jared@pixeloco.com.au>
2021-11-09 23:46:52 +1100

    Updated Site

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20211109.2330';
+const cacheName = 'chirpy-20211109.2346';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
