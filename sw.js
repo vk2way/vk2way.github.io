de9c74555acfce79e69c6c946df2dfe7c7eeeb1e
Jared Quinn <jared@pixeloco.com.au>
2021-11-11 16:15:37 +1100

    Add affiliate warning

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20211111.1607';
+const cacheName = 'chirpy-20211111.1615';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
